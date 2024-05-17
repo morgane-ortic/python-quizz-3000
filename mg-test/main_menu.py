35f08d2f3237b5e696accee6edf6a382162ac1a9
Morgane Ortic <morgane.ortic@proton.me>
2024-05-17 21:39:36 +0200

    finished basic collision hitboxes in game menu

--- mg-test/main_menu.py
+++ mg-test/main_menu.py
@@ -6,7 +6,7 @@ os.chdir(sourceFileDir)
 
 pygame.init()
 
-win = pygame.display.set_mode((1280,1000))
+win = pygame.display.set_mode((1536,1000))
 
 pygame.display.set_caption("First Game")
 
@@ -46,8 +46,8 @@ char = sprites[24]
 
 clock = pygame.time.Clock()
 
-music = pygame.mixer.music.load('music.mp3')
-pygame.mixer.music.play(-1)
+# music = pygame.mixer.music.load('music.mp3')
+# pygame.mixer.music.play(-1)
 
 score = 0
 
@@ -98,9 +98,25 @@ class House(object):
         self.rect = pygame.Rect(x, y, width, height)
 
 # Create some houses
-houses = [
-    House(387, 66, 616, 243), # We add 9px to starting x and substract 18px to rectangle width to make it look good with player sprite
-    House(537, 359, 318, 49)        # We substract 48px from height to show player except feet in front of it at the bottom
+houses = [                      # We add 9px to starting x and substract 18px to rectangle width to make it look good with player sprite
+    # Inn
+    House(387, 66, 616, 291),
+    House(537, 359, 318, 97),
+
+    # shop
+    House(243, 551, 330, 243),
+    House(243, 841, 180, 79),
+    House(489, 841, 84, 79),
+    House(243, 794, 1, 48),
+    House(530, 794, 43, 48),
+    House(261, 758, 153, 47),
+
+    # stables
+    House(1159, 286, 126, 167),
+
+    # small house
+    House(777, 575, 222, 217),
+    House(935, 792, 64, 30),
 ]
 
 def redrawGameWindow():
@@ -115,11 +131,11 @@ def redrawGameWindow():
 
 #mainloop
 font = pygame.font.SysFont('comicsans', 30, True)
-man = player(576, 470, 48, 60)          # Starting position and size of player
+man = player(676, 470, 48, 60)          # Starting position and size of player
 shootLoop = 0
 run = True
 while run:
-    clock.tick(27)
+    clock.tick(30)
 
     if shootLoop > 0:
         shootLoop += 1
@@ -137,7 +153,7 @@ while run:
     new_y = man.y
     if keys[pygame.K_LEFT] and man.x > man.vel:
         new_x -= man.vel
-    elif keys[pygame.K_RIGHT] and man.x < 1280 - man.width - man.vel:
+    elif keys[pygame.K_RIGHT] and man.x < 1536 - man.width - man.vel:
         new_x += man.vel
     if keys[pygame.K_UP] and man.y > man.velY:
         new_y -= man.velY
@@ -145,7 +161,7 @@ while run:
         new_y += man.velY
 
     # Check if the new position would collide with a house
-    player_rect = pygame.Rect(new_x, new_y, man.width, man.height)
+    player_rect = pygame.Rect(new_x, new_y + 42, man.width, man.height - 42)
     if not any(player_rect.colliderect(house.rect) for house in houses):
         # If not colliding with any house, update the player's position
         man.x = new_x
@@ -155,7 +171,7 @@ while run:
         man.left = True
         man.right = False
         man.standing = False
-    elif keys[pygame.K_RIGHT] and man.x < 1280 - man.width - man.vel:
+    elif keys[pygame.K_RIGHT] and man.x < 1536 - man.width - man.vel:
         man.right = True
         man.left = False
         man.standing = False

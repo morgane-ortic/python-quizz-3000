246ac8f88827275b0e900c61a37e7660886a35f9
ramon <ramonpolanski@gmail.com>
2024-05-28 14:40:31 +0200

    changed creater db to user_questions.db

--- question_creator_dastin.py
+++ question_creator_dastin.py
@@ -6,7 +6,7 @@ import tkinter as tk
 import sqlite3
 
 def createTableQuestion():
-    conn = sqlite3.connect('user_info.db')
+    conn = sqlite3.connect('user_questions.db')
     c = conn.cursor()
     c.execute( '''
                 CREATE TABLE IF NOT EXISTS Question(
@@ -23,7 +23,7 @@ createTableQuestion()
 
 def newquestion(name, question, correctoutput):
     print(name, question, correctoutput)
-    conn = sqlite3.connect('user_info.db')
+    conn = sqlite3.connect('user_questions.db')
     cursor = conn.cursor()
     insert_query = '''
     INSERT INTO Question (name, quiz_question, correct_output)
@@ -97,7 +97,7 @@ def editquestinwindow(id):
     window.mainloop()
 
 def allquestionwinow():
-    conn = sqlite3.connect('user_info.db')
+    conn = sqlite3.connect('user_questions.db')
     c = conn.cursor()
     c.execute('SELECT id, name FROM Question')
     allquest = c.fetchall()

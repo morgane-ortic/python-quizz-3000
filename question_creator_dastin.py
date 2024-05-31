bb3800a0d8ff7e02efe0b125fe6a64cf04966468
StudentDastin <163409328+StudentDastin@users.noreply.github.com>
2024-05-31 09:56:56 +0200

    Update question_creator_dastin.py

--- question_creator_dastin.py
+++ question_creator_dastin.py
@@ -1,129 +1,104 @@
 from tkinter import *
 #import customtkinter
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image
 from tkinter import messagebox 
 import tkinter as tk
-import sqlite3
-
-def createTableQuestion():
-    conn = sqlite3.connect('user_questions.db')
-    c = conn.cursor()
-    c.execute( '''
-                CREATE TABLE IF NOT EXISTS Question(
-                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 name TEXT NOT NULL,
-                 quiz_question TEXT NOT NULL,
-                 correct_output TEXT NOT NULL)
-              ''')
-    conn.commit()
-    conn.close()
-
-createTableQuestion()
+import json
+import os
 
 
-def newquestion(name, question, correctoutput):
-    print(name, question, correctoutput)
-    conn = sqlite3.connect('user_questions.db')
-    cursor = conn.cursor()
-    insert_query = '''
-    INSERT INTO Question (name, quiz_question, correct_output)
-    VALUES (?, ?, ?)
-    '''
+def newquestion(level1, question1, code1, output1, question2, code2, output2, question3, code3, output3):
+    print(level1, question1, output1, question2, output2,  question3, output3)
+    daten1 = {
+    "question": question1,
+    "code": code1,
+    "output": output1,
+ 
+}
+    daten2 = {
+    "question": question2,
+    "code": code2,
+    "output": output2,
+ 
+}
+    daten3 = {
+    "question": question3,
+    "code": code3,
+    "output": output3,
+ 
+}
+    name = "lvl" + level1 + ".json"
+    daten_liste = [daten1, daten2, daten3]
+    with open( name, 'w', encoding='utf-8') as json_datei:
+        json.dump(daten_liste, json_datei, ensure_ascii=False, indent=4)
     
-    try:
-        cursor.execute(insert_query, (name, question, correctoutput))
-        conn.commit()
-        print("Successful")
-    except sqlite3.Error as e:
-        print(f"Ein Fehler ist aufgetreten: {e}")
-    finally:
-        # Verbindung zur Datenbank schließen
-        conn.close()    
-
-def editquestion(id, name, question, correctoutput):
-    print(id, name, question, correctoutput)
-    return 0
-
 def cratequestionwindow():
     print("createquestionwindow")
     window = tk.Tk()
     window.title("quizcreator")
     window.geometry("1300x800")
-    print("allquestion")
 
-    label_name = tk.Label(window, text="Name:")
-    label_name.pack(pady=10)
-    textfeld_name = tk.Entry(window)
-    textfeld_name.pack(pady=10)    
-
-    label_question = tk.Label(window, text="Question:")
-    label_question.pack(pady=10)
-    textfeld_question = tk.Entry(window)
-    textfeld_question.pack(pady=10)
-
-    label_correctoutput = tk.Label(window, text="Correct Output")
-    label_correctoutput.pack(pady=10)
-    textfeld_correctoutput = tk.Entry(window)
-    textfeld_correctoutput.pack(pady=10)
-
-    button_commit = tk.Button(window, text="commit", command=lambda: newquestion(name=textfeld_name.get(), question=textfeld_question.get(), correctoutput=textfeld_correctoutput.get() ))
-    button_commit.pack(pady=10)
-    window.mainloop()
-
-def editquestinwindow(id):
-    print("createquestionwindow")
-    window = tk.Tk()
-    window.title("quizcreator")
-    window.geometry("1300x800")
-    print("allquestion")
-
-    label_name = tk.Label(window, text="Name:")
-    label_name.pack(pady=10)
-    textfeld_name = tk.Entry(window)
-    textfeld_name.pack(pady=10)    
-
-    label_question = tk.Label(window, text="Question:")
-    label_question.pack(pady=10)
-    textfeld_question = tk.Entry(window)
-    textfeld_question.pack(pady=10)
-
-    label_correctoutput = tk.Label(window, text="Correct Output")
-    label_correctoutput.pack(pady=10)
-    textfeld_correctoutput = tk.Entry(window)
-    textfeld_correctoutput.pack(pady=10)
-
-    button_save = tk.Button(window, text="save", command=lambda: editquestion(id=id, name=textfeld_name.get(), question=textfeld_question.get(), correctoutput=textfeld_correctoutput.get() ))
-    button_save.pack(pady=10)
-    window.mainloop()
-
-def allquestionwinow():
-    conn = sqlite3.connect('user_questions.db')
-    c = conn.cursor()
-    c.execute('SELECT id, name FROM Question')
-    allquest = c.fetchall()
-    conn.close()
-    x = 0
-    window = tk.Tk()
-    window.title("quizcreator")
-    window.geometry("1300x800")
-    button_commit_list = []
-    label_name_list = []
-    while x< allquest.__len__():
-        label_name = tk.Label(window, text= allquest[x][1])
-        label_name_list.append(label_name)
-        label_name_list[x].pack(pady=10)
-        button = tk.Button(window, text="edit", command= lambda: editquestinwindow(allquest[x][0]))
-        button_commit_list.append(button)
-        button_commit_list[x].pack(pady=10)
-        print(x)
-        x = x+1
+    label_level1 = tk.Label(window, text="level:")
+    label_level1.pack(pady=10)
+    textfeld_level1 = tk.Entry(window)
+    textfeld_level1.pack(pady=10)    
     
+    label_question1 = tk.Label(window, text="Question:")
+    label_question1.pack(pady=10)
+    textfeld_question1 = tk.Entry(window)
+    textfeld_question1.pack(pady=10)
+
+    label_code1 = tk.Label(window, text="code")
+    label_code1.pack(pady=10)
+    textfeld_code1 = tk.Entry(window)
+    textfeld_code1.pack(pady=10)
+
+    label_correctoutput1 = tk.Label(window, text="Correct Output")
+    label_correctoutput1.pack(pady=10)
+    textfeld_correctoutput1 = tk.Entry(window)
+    textfeld_correctoutput1.pack(pady=10)
+    
+   
+    label_question2 = tk.Label(window, text="Question:")
+    label_question2.pack(pady=10)
+    textfeld_question2 = tk.Entry(window)
+    textfeld_question2.pack(pady=10)
+
+    label_code2 = tk.Label(window, text="code")
+    label_code2.pack(pady=10)
+    textfeld_code2 = tk.Entry(window)
+    textfeld_code2.pack(pady=10)
+
+    label_correctoutput2 = tk.Label(window, text="Correct Output")
+    label_correctoutput2.pack(pady=10)
+    textfeld_correctoutput2 = tk.Entry(window)
+    textfeld_correctoutput2.pack(pady=10)
+
+
+    label_question3 = tk.Label(window, text="Question:")
+    label_question3.pack(pady=10)
+    textfeld_question3 = tk.Entry(window)
+    textfeld_question3.pack(pady=10)
+
+    label_code3 = tk.Label(window, text="code")
+    label_code3.pack(pady=10)
+    textfeld_code3 = tk.Entry(window)
+    textfeld_code3.pack(pady=10)
+    
+    label_correctoutput3 = tk.Label(window, text="Correct Output")
+    label_correctoutput3.pack(pady=10)
+    textfeld_correctoutput3 = tk.Entry(window)
+    textfeld_correctoutput3.pack(pady=10)
+
+    submit_button = tk.Button(window, text="Daten speichern", command=lambda: newquestion(
+        textfeld_level1.get(), textfeld_question1.get(), textfeld_code1.get(), textfeld_correctoutput1.get(),
+        textfeld_question2.get(), textfeld_code2.get(), textfeld_correctoutput2.get(),
+        textfeld_question3.get(), textfeld_code3.get(), textfeld_correctoutput3.get()
+    ))
+    submit_button.pack(pady=20)
+    window.geometry("1600x1200")
     window.mainloop()
-    # liste aus label mit den namen der question
-    # neben dem labels folgende buttons 
-        # edit, delete
-            # mybe all that delete go to archiv
-    return 0
+
 
 def main_screen():
     window = tk.Tk()
@@ -131,8 +106,6 @@ def main_screen():
     window.geometry("1300x800")
     create_button = tk.Button(window, text="Create", command=cratequestionwindow)
     create_button.pack()
-    edit_button = tk.Button(window, text="Edit", command=allquestionwinow)
-    edit_button.pack()
     window.mainloop() 
 
 
